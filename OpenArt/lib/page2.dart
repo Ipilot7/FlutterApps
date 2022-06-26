38fb7a629c0cd142be3ad200943b0b72724e529d
Baxtiyor <botiraliyevbaxtiyor@yandex.ru>
2022-06-26 10:15:43 +0300

    second page is added

--- OpenArt/lib/page2.dart
+++ OpenArt/lib/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:page3/constant.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyWidget(),
-    
     );
   }
 }
@@ -20,17 +20,212 @@ class MyApp extends StatelessWidget {
 class MyWidget extends StatefulWidget {
   const MyWidget({Key? key}) : super(key: key);
 
-
   @override
   State<MyWidget> createState() => _MyWidgetState();
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  final scafoldColor = const Color(0xfff8f8f8);
+  final textColor = const Color(0xff333333);
+  final _gradient = const LinearGradient(
+    colors: [Color(0xff0038f5), Color(0xff9f03ff)],
+    transform: GradientRotation(45),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: scafoldColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: scafoldColor,
+        title: Image.asset('assets/Logo.png', height: 37),
+        centerTitle: false,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/Search.png',
+                width: 18,
+                height: 18,
+              )),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu, color: Colors.black),
+            color: scafoldColor,
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 23, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Add wallet to pay',
+                    style: kTextstyle(size: 24, color: textColor)),
+                SizedBox(
+                  height: 4,
+                ),
+                Text('Easy to sell your Digital Art with 3 step',
+                    style: kTextstyle(
+                        size: 16,
+                        fontWeight: FontWeight.normal,
+                        color: textColor)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 36, bottom: 26),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    _circles('1', Colors.white, _gradient),
+                    Text('Sellect'),
+                  ],
+                ),
+                Image.asset(
+                  'assets/line.png',
+                  width: 68,
+                ),
+                Column(
+                  children: [
+                    _circles('2', Colors.black45),
+                    const Text(
+                      'Scan',
+                      style: TextStyle(color: Color(0xff888888)),
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  'assets/line.png',
+                  width: 68,
+                ),
+                Column(
+                  children: [
+                    _circles('3', Colors.black45),
+                    const Text('Confirm',
+                        style: TextStyle(color: Color(0xff888888))),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          _itemWallet('Bank wallet'),
+          _itemWallet('Coin wallet'),
+          _itemWallet('Wallet connect'),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Image.asset(
+              'assets/biglogo.png',
+              width: 177,
+              height: 46,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _textEnd('The ', FontWeight.w200),
+              _textEnd('New ', FontWeight.w300),
+              _textEnd('Creative ', FontWeight.w500),
+              _textEnd('Economy', FontWeight.w900),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            margin:
+                const EdgeInsets.only(bottom: 15, top: 29, left: 15, right: 15),
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), gradient: _gradient),
+            child: Text(
+              'Earn now',
+              style: kTextstyle(size: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            margin: const EdgeInsets.only(left: 15, right: 15),
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xff0038F5),
+                ),
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white),
+            child: Text(
+              'Discover more',
+              style: kTextstyle(
+                  size: 20, fontWeight: FontWeight.w800, color: Colors.black54),
+            ),
+          )
+        ],
+      ),
     );
-    
+  }
+
+//------------------------------------------------------------------------
+  Text _textEnd(String text, FontWeight fontWeight) {
+    return Text(
+      text,
+      style:
+          TextStyle(color: Colors.black, fontSize: 26, fontWeight: fontWeight),
+    );
+  }
+
+  Padding _itemWallet(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 343,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: ListTile(
+          leading: Container(
+            alignment: Alignment.center,
+            width: 60,
+            height: 100,
+            decoration: BoxDecoration(
+                color: const Color(0xffDCDCDC),
+                borderRadius: BorderRadius.circular(20)),
+            child: const Text(
+              '+',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w300),
+            ),
+          ),
+          title: Text(
+            text,
+            style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Color(0xff555555)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _circles(String text, Color? color, [Gradient? gradient]) {
+    return Container(
+      alignment: Alignment.center,
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle, gradient: gradient, color: Color(0xffDCDCDC)),
+      child: Text(
+        text,
+        style:
+            TextStyle(color: color, fontSize: 14, fontWeight: FontWeight.w700),
+      ),
+    );
   }
 }
