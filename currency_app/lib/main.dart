a8a09b5d51efd1ebce815258e500031a60f4c024
Baxtiyor <botiraliyevbaxtiyor@yandex.ru>
2022-07-02 10:51:42 +0300

    currency

--- currency_app/lib/main.dart
+++ currency_app/lib/main.dart
@@ -1,31 +1,35 @@
-import 'package:flutter/material.dart';
-import 'utils.dart';
-import 'currency_model.dart';
-import 'package:http/http.dart';
 import 'dart:convert';
 import 'dart:io';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Currency(),
-    ),
-  );
+import 'package:currency_app/currency_page.dart';
+import 'package:currency_app/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart';
+import 'utils.dart';
+import 'currency_model.dart';
+// import 'package:lesson1/utils/constants.dart';
+// import 'package:lesson1/utils/routes.dart';
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: ComparePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class Currency extends StatefulWidget {
-  const Currency({Key? key}) : super(key: key);
+class ComparePage extends StatefulWidget {
+  const ComparePage({Key? key}) : super(key: key);
 
   @override
-  State<Currency> createState() => _Currency();
+  State<ComparePage> createState() => _ComparePageState();
 }
-
-class _Currency extends State<Currency> {
+final List<CurrencyModel> listCurrency = [];
+class _ComparePageState extends State<ComparePage> {
   final TextEditingController _editingControllerTop = TextEditingController();
-  final TextEditingController _editingControllerBottom =
-      TextEditingController();
-  final List<CurrencyModel> _listCurrency = [];
+  final TextEditingController _editingControllerBottom = TextEditingController();
+  final FocusNode _topFocus = FocusNode();
+  final FocusNode _bottomFocus = FocusNode();
+  
   CurrencyModel? topCur;
   CurrencyModel? bottomCur;
 
@@ -33,10 +37,32 @@ class _Currency extends State<Currency> {
   void initState() {
     super.initState();
     _editingControllerTop.addListener(() {
-      print(_editingControllerTop.text);
+      if (_topFocus.hasFocus) {
+        setState(() {
+          if (_editingControllerTop.text.isNotEmpty) {
+            double sum = double.parse(topCur?.rate ?? '0') /
+                double.parse(bottomCur?.rate ?? '0') *
+                double.parse(_editingControllerTop.text);
+            _editingControllerBottom.text = sum.toStringAsFixed(2);
+          } else {
+            _editingControllerBottom.clear();
+          }
+        });
+      }
     });
     _editingControllerBottom.addListener(() {
-      print(_editingControllerBottom.text);
+      if (_bottomFocus.hasFocus) {
+        setState(() {
+          if (_editingControllerBottom.text.isNotEmpty) {
+            double sum = double.parse(bottomCur?.rate ?? '0') /
+                double.parse(topCur?.rate ?? '0') *
+                double.parse(_editingControllerBottom.text);
+            _editingControllerTop.text = sum.toStringAsFixed(2);
+          } else {
+            _editingControllerTop.clear();
+          }
+        });
+      }
     });
   }
 
@@ -44,13 +70,14 @@ class _Currency extends State<Currency> {
   void dispose() {
     _editingControllerTop.dispose();
     _editingControllerBottom.dispose();
+    _topFocus.dispose();
+    _bottomFocus.dispose();
     super.dispose();
   }
 
   Future<bool?> _loadData() async {
     try {
-      var response =
-          await get(Uri.parse('https://cbu.uz/uz/arkhiv-kursov-valyut/json/'));
+      var response = await get(Uri.parse('https://cbu.uz/uz/arkhiv-kursov-valyut/json/'));
       if (response.statusCode == 200) {
         for (final item in jsonDecode(response.body)) {
           var model = CurrencyModel.fromJson(item);
@@ -59,7 +86,7 @@ class _Currency extends State<Currency> {
           } else if (model.ccy == 'RUB') {
             bottomCur = model;
           }
-          _listCurrency.add(model);
+          listCurrency.add(model);
         }
         return true;
       } else {
@@ -88,6 +115,7 @@ class _Currency extends State<Currency> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xff1f2235),
       body: SafeArea(
         child: Padding(
@@ -104,8 +132,7 @@ class _Currency extends State<Currency> {
                       children: [
                         TextSpan(
                           text: 'Wellcome Back',
-                          style:
-                              kTextStyle(size: 20, fontWeight: FontWeight.bold),
+                          style: kTextStyle(size: 20, fontWeight: FontWeight.bold),
                         ),
                       ],
                     ),
@@ -131,91 +158,99 @@ class _Currency extends State<Currency> {
                 ],
               ),
               FutureBuilder(
-                future: _loadData(),
-                builder: ((context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(14),
-                      margin: const EdgeInsets.symmetric(vertical: 25),
-                      decoration: BoxDecoration(
-                        color: const Color(0xff2d334d),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Exchange',
-                                style: kTextStyle(
-                                    size: 16, fontWeight: FontWeight.w600),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                iconSize: 20,
-                                icon: const Icon(
-                                  Icons.settings,
-                                  size: 20,
-                                  color: Colors.white,
+                  future: listCurrency.isEmpty ? _loadData() : null,
+                  builder: ((context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(14),
+                        margin: const EdgeInsets.symmetric(vertical: 25),
+                        decoration: BoxDecoration(
+                          color: const Color(0xff2d334d),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Exchange',
+                                  style: kTextStyle(size: 16, fontWeight: FontWeight.w600),
                                 ),
-                              )
-                            ],
-                          ),
-                          Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Column(
-                                children: [
-                                  _itemExch(_editingControllerTop, topCur),
-                                  const SizedBox(
-                                    height: 15,
+                                IconButton(
+                                  onPressed: () {},
+                                  iconSize: 20,
+                                  icon: const Icon(
+                                    Icons.settings,
+                                    size: 20,
+                                    color: Colors.white,
                                   ),
-                                  _itemExch(
-                                      _editingControllerBottom, bottomCur),
-                                ],
-                              ),
-                              Container(
-                                height: 35,
-                                width: 35,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: const Color(0xff2d334d),
-                                  shape: BoxShape.circle,
-                                  border: Border.all(color: Colors.white12),
+                                )
+                              ],
+                            ),
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    _itemExch(_editingControllerTop, topCur, _topFocus),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    _itemExch(_editingControllerBottom, bottomCur, _bottomFocus),
+                                  ],
                                 ),
-                                child: const Icon(
-                                  Icons.currency_exchange,
-                                  color: Colors.white,
-                                  size: 20,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Expanded(
-                      child: Center(
-                        child: Text(
-                          'Error',
-                          style: kTextStyle(size: 18),
+                                InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      var model = topCur?.copyWith();
+                                      topCur = bottomCur?.copyWith();
+                                      bottomCur = model;
+                                      _editingControllerTop.clear();
+                                      _editingControllerBottom.clear();
+                                    });
+                                  },
+                                  child: Container(
+                                    height: 35,
+                                    width: 35,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff2d334d),
+                                      shape: BoxShape.circle,
+                                      border: Border.all(color: Colors.white12),
+                                    ),
+                                    child: const Icon(
+                                      Icons.currency_exchange,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
                         ),
-                      ),
-                    );
-                  } else {
-                    return const Expanded(
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          color: Colors.white,
+                      );
+                    } else if (snapshot.hasError) {
+                      return Expanded(
+                        child: Center(
+                          child: Text(
+                            'Error',
+                            style: kTextStyle(size: 18),
+                          ),
                         ),
-                      ),
-                    );
-                  }
-                }),
-              ),
+                      );
+                    } else {
+                      return const Expanded(
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    }
+                  }))
             ],
           ),
         ),
@@ -223,13 +258,11 @@ class _Currency extends State<Currency> {
     );
   }
 
-  Widget _itemExch(TextEditingController controller, CurrencyModel? model) {
+  Widget _itemExch(TextEditingController controller, CurrencyModel? model, FocusNode focusNode) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white12),
-          borderRadius: BorderRadius.circular(15)),
+      decoration: BoxDecoration(border: Border.all(color: Colors.white12), borderRadius: BorderRadius.circular(15)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -239,39 +272,37 @@ class _Currency extends State<Currency> {
               Flexible(
                 child: TextField(
                   controller: controller,
+                  focusNode: focusNode,
                   style: kTextStyle(size: 24, fontWeight: FontWeight.bold),
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     isDense: true,
                     hintText: '0.00',
-                    hintStyle:
-                        kTextStyle(size: 24, fontWeight: FontWeight.bold),
+                    hintStyle: kTextStyle(size: 24, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
               GestureDetector(
-                onTap: () {},
+                // onTap: () => Navigator.pushNamed(context, Routes.currencyPage),
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: ((context) => const CurrencyPage()))),
                 child: Container(
                   padding: const EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: const Color(0xff10a4d4)),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: const Color(0xff10a4d4)),
                   child: Row(children: [
-                    // ClipRRect(
-                    //   borderRadius: BorderRadius.circular(10),
-                    //   child: Image.asset(
-                    //     'assets/img_${model?.ccy ?? 'usd'}.png',
-                    //     height: 20,
-                    //     width: 20,
-                    //   ),
-                    // ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: SvgPicture.asset(
+                        'assets/${model?.ccy?.substring(0, 2).toLowerCase()}.svg',
+                        height: 20,
+                        width: 20,
+                      ),
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(left: 5, right: 10),
                       child: Text(
                         model?.ccy ?? 'UNK',
-                        style:
-                            kTextStyle(size: 16, fontWeight: FontWeight.w600),
+                        style: kTextStyle(size: 16, fontWeight: FontWeight.w600),
                       ),
                     ),
                     const Icon(
@@ -285,9 +316,8 @@ class _Currency extends State<Currency> {
             ],
           ),
           Text(
-            '0.00',
-            style:
-                kTextStyle(fontWeight: FontWeight.w600, color: Colors.white54),
+            controller.text.isNotEmpty ? (double.parse(controller.text) * 0.05).toStringAsFixed(2) : '0.00',
+            style: kTextStyle(fontWeight: FontWeight.w600, color: Colors.white54),
           )
         ],
       ),
